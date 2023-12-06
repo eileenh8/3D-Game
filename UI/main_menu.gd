72cd5a35f254ef0d09cdcfe2a1e3e4fa8a1fc96b
eileenh8 <eileen.henriquez@icloud.com>
2023-12-05 20:17:05 -0500

    update

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
 
-func _on_restart_pressed():
+
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
